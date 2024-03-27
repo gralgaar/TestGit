bdc525fcd72cc63613bc39cea1c1e7815282edae
tokotun <ershov.roma.2016@yandex.ru>
2024-03-27 12:03:23 +0300

    Lesson 14,  task 1 add print Triangle

--- home_work/lesson014/task01/Triangle.java
+++ home_work/lesson014/task01/Triangle.java
@@ -6,4 +6,17 @@ public class Triangle extends Shape {
         super(size);
         System.out.println("ТРЕУГОЛЬНИК");
     }
+
+    public void print() {
+        int size = this.size;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size * 2 + 1; j++) {
+                if (j == size - 1 - i) System.out.print("/");
+                else if (j == size + i + 1) System.out.print("\\");
+                else if (i == size - 1) System.out.print("_");
+                else System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
 }
