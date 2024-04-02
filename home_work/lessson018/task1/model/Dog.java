98523ef1ccabca2d5a0a08ef0e5444916278f9dd
tokotun <ershov.roma.2016@yandex.ru>
2024-04-02 20:48:01 +0300

    Lesson 18,  task 01 start

---
+++ home_work/lessson018/task1/model/Dog.java
@@ -0,0 +1,14 @@
+package home_work.lessson018.task1.model;
+
+
+public class Dog extends Animal {
+    public static final String DOG_CLASS_NAME = "Dog";
+
+    public Dog() {
+        super(DOG_CLASS_NAME, "woof");
+    }
+
+    public void woof() {
+        sound();
+    }
+}
