d3d22873e1cb9609db91f6d7e6307ef046380bda
tokotun <ershov.roma.2016@yandex.ru>
2024-05-18 13:28:08 +0300

    Lesson 25 task 01

---
+++ home_work/lesson025/task1/anonimus/EquilateralShape.java
@@ -0,0 +1,12 @@
+package home_work.lesson025.task1.anonimus;
+
+public interface EquilateralShape {
+    String EMPTY_ELEMENT = " ";
+    String EMPTY_STRING = "";
+    char NEW_LINE_SYMBOL = '\n';
+
+//    public int getLength();
+
+
+    public String createShapeString();
+}
