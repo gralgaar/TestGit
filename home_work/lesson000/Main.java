3c55c76f694c762a002aab5e9424e76a69d3e7bb
tokotun <ershov.roma.2016@yandex.ru>
2024-05-22 09:45:26 +0300

    Lesson 00 task 0

---
+++ home_work/lesson000/Main.java
@@ -0,0 +1,12 @@
+package home_work.lesson000;
+
+public class Main {
+    public static void main(String[] args) {
+        Temp temp = new Temp();
+        System.out.println(temp.i + "\n");
+
+        temp.add();
+        System.out.println(temp.i);
+    }
+
+}
